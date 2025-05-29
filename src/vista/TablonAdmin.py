aa06c7025ac9eea6dc33f9201d4925cd91a34fa4
ngenii00 <ngenii00@estudiantes.unileon.es>
2025-05-29 17:05:58 +0200

    funciona

--- src/vista/TablonAdmin.py
+++ src/vista/TablonAdmin.py
@@ -9,16 +9,3 @@ class TablonAdmin(VistaNavegable, Form):
         self.setupUi(self)
         self.conectar_botones_navegacion()
 
-
-
-    aceptar_clicked = pyqtSignal()
-    volver_clicked = pyqtSignal()
-    recuperar_clicked = pyqtSignal()
-
-    def __init__(self):
-        super().__init__()
-        self.setupUi(self) 
-
-        self.Boton.clicked.connect(self.aceptar_clicked)
-        self.BotonVolver.clicked.connect(self.volver_clicked)
-        self.BotonRecuperar.clicked.connect(self.recuperar_clicked)
