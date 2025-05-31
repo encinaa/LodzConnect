82cf6c28e2126ce852b6393387000a15155bd7b1
enciina <mdiezd09@estudiantes.unileon.es>
2025-05-31 16:17:35 +0200

    va las publis

--- src/modelo/conexion/Conexion.py
+++ src/modelo/conexion/Conexion.py
@@ -1,7 +1,7 @@
 import jaydebeapi
 
 class Conexion:
-    def __init__(self, host='localhost', database='UniConecta', user='root', password='Zepolitte00.:)'):
+    def __init__(self, host='localhost', database='UniConecta', user='root', password='Ponferrada.55'):
         self._host = host
         self._database = database
         self._user = user
--- src/modelo/dao/PublicacionDAO.py
+++ src/modelo/dao/PublicacionDAO.py
@@ -1,4 +1,5 @@
 from src.modelo.conexion.Conexion import Conexion
+from src.modelo.vo.PublicacionVO import PublicacionVO
 
 class PublicacionDAO:
     def __init__(self):
@@ -14,3 +15,21 @@ class PublicacionDAO:
             publicacion.descripcion  # ✅ ahora hay 4 valores
         ))
 
+    def obtener_todas_publicaciones(self):
+        sql = "SELECT idPublic, fecha, listaEtiquetados, cuentaOrigen, descripcion FROM Publicacion ORDER BY fecha DESC"
+        self.cursor.execute(sql)
+        filas = self.cursor.fetchall()
+
+        publicaciones = []
+        for fila in filas:
+            lista_etiquetados = fila[2].split(',') if fila[2] else []
+            publicacion = PublicacionVO(
+                idPublic=fila[0],
+                fecha=fila[1],
+                listaEtiquetados=lista_etiquetados,
+                cuentaOrigen=fila[3],
+                descripcion=fila[4]
+            )
+            publicaciones.append(publicacion)
+
+        return publicaciones
