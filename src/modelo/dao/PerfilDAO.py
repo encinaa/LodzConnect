29c8d6a3e187bf6ab5969ff27e2ab736705d3c07
enciina <mdiezd09@estudiantes.unileon.es>
2025-06-01 02:57:33 +0200

    YA ESTA EL PERFIL, solo queda test

--- src/modelo/dao/PerfilDAO.py
+++ src/modelo/dao/PerfilDAO.py
@@ -18,8 +18,20 @@ class PerfilDAO:
     def actualizar_perfil(self, correo, nueva_descripcion, nuevas_actividades):
         cursor = self.conn.getCursor()
         try:
-            cursor.execute("UPDATE perfil SET descripcion = ?, listaActividades = ? WHERE correo = ?", (nueva_descripcion, nuevas_actividades, correo))
+            cursor.execute("SELECT COUNT(*) FROM perfil WHERE correo = ?", (correo,))
+            existe = cursor.fetchone()[0] > 0
+
+            if existe:
+                cursor.execute(
+                    "UPDATE perfil SET descripcion = ?, listaActividades = ? WHERE correo = ?",
+                    (nueva_descripcion, nuevas_actividades, correo)
+                )
+            else:
+                cursor.execute(
+                    "INSERT INTO perfil (descripcion, listaActividades, correo) VALUES (?, ?, ?)",
+                    (nueva_descripcion, nuevas_actividades, correo)
+                )
         except Exception as e:
-            print(f"Error al actualizar perfil: {e}")
+            print(f"Error al actualizar o insertar perfil: {e}")
         finally:
             cursor.close()
